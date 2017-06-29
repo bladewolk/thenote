a1acb6cae55dd6b85594416b1a3060ded9d62d31
Alex <bladewolk@gmail.com>
2017-06-29 13:55:09 +0300

    refactor export

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
-use Chumper\Zipper\Facades\Zipper;
 use Illuminate\Http\Request;
 use File;
-use SoapBox\Formatter\Formatter;
 
 
 class ExportController extends Controller
@@ -25,54 +23,16 @@ class ExportController extends Controller
 
     /**
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse|void
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
     public function export(Request $request){
-        if (File::exists(public_path('export.zip')))
-            File::delete(public_path('export.zip'));
 
-        if ($request->format == 'txt')
-            return $this->exportTXT();
-        return $this->exportXML();
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
-     */
-    public function exportTXT(){
-        File::cleanDirectory(public_path('export'));
-        $notes = Note::all();
-        foreach ($notes as $note){
-            $filename = public_path('export/'.$note->id.'.txt');
-            file_put_contents($filename, $note->description);
+        if ($request->get('format') == 'txt'){
+            Note::exportTXT();
+            return response()->download(public_path('export.zip'));
         }
 
-        $imagePath = glob(public_path('uploads/*'));
-        $txtPath = glob(public_path('export/*'));
-        Zipper::make(public_path('export.zip'))
-            ->add(array_merge($imagePath, $txtPath))
-            ->close();
-
-        return response()->download(public_path('export.zip'));
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
-     */
-    public function exportXML(){
-        File::cleanDirectory(public_path('export'));
-        $notes = Note::all()->toJson();
-        $formatter = Formatter::make($notes, Formatter::JSON);
-        $xml = $formatter->toXml();
-        file_put_contents(public_path('export/'.'notes.xml'), $xml);
-
-        $imagePath = glob(public_path('uploads/*'));
-        $xmlPath = [0 => public_path('export/'.'notes.xml')];
-
-        Zipper::make(public_path('export.zip'))
-            ->add(array_merge($imagePath, $xmlPath))
-            ->close();
-
+        Note::exportXML();
         return response()->download(public_path('export.zip'));
     }
 }
--- app/Models/Note.php
+++ app/Models/Note.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Chumper\Zipper\Facades\Zipper;
+use File;
+use SoapBox\Formatter\Formatter;
 
 class Note extends Model
 {
@@ -10,5 +13,41 @@ class Note extends Model
 
     public function pictures(){
         return $this->morphMany(Picture::class, 'picturetable');
+    }
+
+    public static function ExportTXT(){
+        if (File::exists(public_path('export.zip')))
+            File::delete(public_path('export.zip'));
+
+        File::cleanDirectory(public_path('export'));
+        $notes = self::all();
+        foreach ($notes as $note){
+            $filename = public_path('export/'.$note->id.'.txt');
+            file_put_contents($filename, $note->description);
+        }
+
+        $imagePath = glob(public_path('uploads/*'));
+        $txtPath = glob(public_path('export/*'));
+        Zipper::make(public_path('export.zip'))
+            ->add(array_merge($imagePath, $txtPath))
+            ->close();
+    }
+
+    public static function ExportXML(){
+        if (File::exists(public_path('export.zip')))
+            File::delete(public_path('export.zip'));
+
+        File::cleanDirectory(public_path('export'));
+        $notes = self::all()->toJson();
+        $formatter = Formatter::make($notes, Formatter::JSON);
+        $xml = $formatter->toXml();
+        file_put_contents(public_path('export/'.'notes.xml'), $xml);
+
+        $imagePath = glob(public_path('uploads/*'));
+        $xmlPath = [0 => public_path('export/'.'notes.xml')];
+
+        Zipper::make(public_path('export.zip'))
+            ->add(array_merge($imagePath, $xmlPath))
+            ->close();
     }
 }
